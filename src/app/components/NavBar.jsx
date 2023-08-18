413366a760c9a224c5e8c960cadf2910435757d1
HeitorBMarini <mariniheitor@gmail.com>
2023-08-18 11:16:45 -0300

    Alterando cores na NavBar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,28 +1,31 @@
 import Link from "next/link";
 
+
+
+
 export default function NavBar({ active }) {
     return (
         <nav className="flex justify-between items-center bg-slate-900 px-6 py-4">
-            <ul className="flex gap-20 items-end">
+            <ul className="flex gap-60 items-center">
                 <li>
                     <Link href="/">
-                        <h1 className="text-2xl text-slate-100">
-                            MeuJulius
+                        <h1 className="text-2xl text-slate-50 text-red-400">
+                            Projeto HB
                         </h1>
                     </Link>
                 </li>
                 <li>
-                    <Link className={active == "despesas" && "text-slate-100"} href="/despesas">
+                    <Link className={active == "despesas" && "text-slate-500 text-gray-500"} href="/despesas">
                         despesas
                     </Link>
                 </li>
                 <li>
-                    <Link className={active == "contas" && "text-slate-100"} href="/contas">
+                    <Link className={active == "contas" && "text-slate-100 text-gray-500"} href="/contas">
                         contas
                     </Link>
                 </li>
                 <li>
-                    <Link className={active == "categorias" && "text-slate-100"} href="/categorias">
+                    <Link className={active == "categorias" && "text-slate-100 text-gray-500"} href="/categorias">
                         categorias
                     </Link>
                 </li>
