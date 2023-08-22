4cdc438b36adc4552b8f9dcab77302fd21d0db40
HeitorBMarini <mariniheitor@gmail.com>
2023-08-21 23:00:36 -0300

    começando alterações e usando ícones

--- src/app/contas/page.js
+++ src/app/contas/page.js
@@ -1,19 +1,25 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import {CrediCardIcon} from '@'
 
 export default function Contas() {
   return (
-    <>
-      <NavBar active={"contas"} />
-
+    <div>
+      <NavBar />
       <main className="bg-slate-900 m-20 p-8">
-        <h2>Contas</h2>
+        <h2 className="text-xl">Contas</h2>
+        
+        <div id="data">
+          <div id="data-row" className=" flex justify-between hover:bg-slate-800 p-2 
+          my-2 rounded cursor-pointer"> 
+          <span> I</span>
+          <span>Itaú </span>
+          
+        </div>
+        <span> R$ 100.00</span>
+        </div>
       </main>
-
-
-
-      <Footer active={"contas"} />
-      
-    </>
-  )
+      <Footer />
+    </div>
+  );
 }
